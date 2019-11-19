0a93658795119164a37a7480a8d4674821146c49
mohamed chibani <mohamed.chibani@logigroup.local>
2019-11-19 18:48:14 +0100

    first commit

---
+++ complete-javascript-course-master/3-how-JS-works/starter/script.js
@@ -0,0 +1,87 @@
+ ///////////////////////////////////////
+// Lecture: Hoisting
+
+calculateAge(1996);
+
+
+function calculateAge(year){
+    console.log(2019 - year);
+}
+
+
+retirement(1996);
+var retirement = function(year){
+    console.log(65-(2019-year));
+}
+
+retirement(1996);
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
