866903a7c21cf4cd9e14287aa6e5bcfeab77087c
mohamed chibani <mohamed.chibani@logigroup.local>
2019-11-29 17:03:18 +0100

    section 5 half

---
+++ complete-javascript-course-master/6-budgety/starter/app.js
@@ -0,0 +1,66 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function(){
+
+
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function(){
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue : '.add__value'
+    }
+
+    return {
+        getInput: function(){
+
+            return {
+                type : document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            }
+             
+        }
+    }
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+    var ctrlAddItem = function(){
+        // 1. Get the fieled input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+
+        // Enter button
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    })
+    
+
+})(budgetController, UIController);
+
+
